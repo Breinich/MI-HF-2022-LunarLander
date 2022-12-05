95a9a7ef07eb257d0c07d67d859fb9eb056dbc54
Breinich <bajnokvencel@edu.bme.hu>
2022-12-05 15:18:34 +0100

    space resolution

--- LunarLanderAgentBase.java
+++ LunarLanderAgentBase.java
@@ -7,7 +7,7 @@ public class LunarLanderAgentBase {
     //   1: Y component of the vector pointing to the middle of the platform from the lander
     //   2: X component of the velocity vector of the lander
     //   3: Y component of the velocity vector of the lander
-    static final int[] OBSERVATION_SPACE_RESOLUTION = {0, 0, 0, 0}; // TODO
+    static final int[] OBSERVATION_SPACE_RESOLUTION = {61, 20, 29, 29}; // TODO
 
     final double[][] observationSpace;
     double[][][][][] qTable;
